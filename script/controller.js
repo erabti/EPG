ebacba67350bd9a15275d491ed8e281884122f6f
erabti <ahmed.erabti@yahoo.com>
2016-08-29 08:26:33 +0300

    Add controller.js file to index.Add alert() to the index page when it's load (welcoming). Add show and hide programming statements buttons

---
+++ script/controller.js
@@ -0,0 +1,6 @@
+function welcomeAlert ()
+{
+	isPlay = false;
+	alert("Hello and Welcome to \"Educational Programming Game\" website, here you'll learn some\
+ basic concepts of programming to start, click \"Start\" Button in the Control Panel or\ start programming by select a programming statement!");
+}
