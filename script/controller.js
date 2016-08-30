7a38f555109b83be916919ce69b1209b4ba70804
erabti <ahmed.erabti@yahoo.com>
2016-08-30 07:15:29 +0300

    Main:Add if_statement_buttons and hide them and few other changes.

--- script/controller.js
+++ script/controller.js
@@ -1,6 +1,36 @@
 function welcomeAlert ()
 {
-	isPlay = false;
 	alert("Hello and Welcome to \"Educational Programming Game\" website, here you'll learn some\
  basic concepts of programming to start, click \"Start\" Button in the Control Panel or\ start programming by select a programming statement!");
 }
+
+
+//****************************
+//**Statements controllers**
+//****************************
+
+//buttons objects:
+//statements buttons
+var if_btn = document.getElementById("p_button1");
+var for_btn = document.getElementById("p_button2");
+var switch_btn = document.getElementById("p_button3");
+var while_btn = document.getElementById("p_button4");
+var statements_buttons = [if_btn,for_btn,switch_btn,while_btn]; //in an array
+//getting if buttons and putting them in an array
+var if_statement_buttons = document.getElementsByClassName("if_btns");
+
+function hideButtons (btn_array)
+{
+	for (var i = 0; i < btn_array.length; i++)
+	{
+		btn_array[i].style.visibility="hidden";
+	}
+}
+function showButtons (btn_array)
+{
+	for (var i = 0; i < btn_array.length; i++)
+	{
+		btn_array[i].style.visibility="visible";
+	}
+}
+showButtons(statements_buttons);
